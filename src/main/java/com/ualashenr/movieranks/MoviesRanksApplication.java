094bae372c710364ebef0c106313c0b35e4c48ee
Ualas Henrique <itsfake087@hotmail.com>
2024-01-01 20:23:46 -0300

    Project created

---
+++ src/main/java/com/ualashenr/movieranks/MoviesRanksApplication.java
@@ -0,0 +1,13 @@
+package com.ualashenr.movieranks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesRanksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesRanksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ualashenr/movieranks/MoviesRanksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ualashenr.movieranks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesRanksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
